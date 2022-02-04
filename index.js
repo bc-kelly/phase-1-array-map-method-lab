281cd7634f28e8bb1179364eb52bd7b7239d3095
Bridget Kelly <bckelly197@gmail.com>
2022-02-04 09:29:19 -0500

    completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,69 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials
+// }
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((word) => {
+    const items = word.split(" ");
+    const capitalizedItems = items.map(
+      (item) => item.charAt(0).toUpperCase() + item.slice(1)
+    );
+    const response = capitalizedItems.join(" ");
+    return response;
+  });
+};
+console.log(titleCased());
+
+
+// const titleCased = () => {
+//   return tutorials.map((line) => {
+//     const tokens = line.split(" ");
+//     const capitalizedTokens = tokens.map(
+//       (token) => token.charAt(0).toUpperCase() + token.slice(1)
+//     );
+//     const response = capitalizedTokens.join(" ");
+//     return response;
+//   });
+// };
+
+
+
+// const mySentence = "freeCodeCamp is an awesome resource"
+// const words = mySentence.split(" ");
+
+// const newWords = words.map((word) => { 
+//     return word[0].toUpperCase() + word.substring(1); 
+// }).join(" ");
+// const secondTry = 
+// console.log(newWords);
+
+
+
+
+// const titleCased = tutorials.map(word => word.charAt(0).toUpperCase() + word.substr(1).toLowerCase());
+//  console.log(titleCased());
+
+// function titleCased(tutorials) {
+//   return tutorials.charAt(0).toUpperCase() + string.slice(1);
+// }
+// const caps = tutorials.split(' ').map(capitalize).join(' ');
+// caps;
+
+
+
+// function capitalizeFirstLetter(string) {
+//   return string.charAt(0).toUpperCase() + string.slice(1);
+// }
+// console.log(capitalizeFirstLetter('foo'));
+
+
+// const str = ['captain picard', 'captain hook']
+// function capitalize(str) {
+//   return str.charAt(0).toUpperCase() + str.slice(1);
+// }
+// const caps = str.split(' ').map(capitalize).join(' ');
+// caps; 
+// console.log(caps);
